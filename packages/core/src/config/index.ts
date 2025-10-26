6cf374a86302d0aa6203480be9928ce7d2b5a8c4
Aishwarya B R <aishwarya.b.raghavan@gmail.com>
2025-10-26 21:23:59 +0530

    core config

---
+++ packages/core/src/config/index.ts
@@ -0,0 +1,5 @@
+// packages/core/src/config/index.ts
+
+export * from './integration-config.interface';
+export * from './default-configs';
+export * from './config-merger';
---
+++ packages/core/src/config/integration-config.interface.ts
@@ -0,0 +1,24 @@
+// packages/core/src/config/integration-config.interface.ts
+
+export interface IntegrationConfig {
+    name: string;
+    displayName?: string;
+    tokenManagement: {
+      bufferSeconds: number;
+    };
+    http?: {
+      timeout?: number;
+      retryAttempts?: number;
+      retryDelay?: number;
+      retryBackoff?: number;
+    };
+    rateLimits?: {
+      requestsPerSecond?: number;
+      requestsPerMinute?: number;
+      requestsPerHour?: number;
+    };
+    cache?: {
+      enabled?: boolean;
+      ttl?: number;
+    };
+  }
